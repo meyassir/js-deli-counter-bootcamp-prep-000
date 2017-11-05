358455a21e765287b6a1c8a803ef48b780600958
yasser mimouni <yasmimouni2@gmail.com>
2017-11-05 21:00:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-function takeANumber(currentLine, name)
+function takeANumber(crentLine, name)
 {
   currentLine.push(name);
-  return `Welcome, ${name}. You are number ${currentLine.length} in line.`;
+  return `Welcome, ${name}. You are number ${crentLine.length} in line.`;
 }
 
-function nowServing(currentLine)
+function nowServing(crentLine)
 {
-  if(currentLine.length === 0) return "There is nobody waiting to be served!";
-  return `Currently serving ${currentLine.shift()}.`;
+  if(crentLine.length === 0) return "There is nobody waiting to be served!";
+  return `Currently serving ${crentLine.shift()}.`;
+}
 
+function currentLine(crentLine)
+{
+  if (crentLine.length === 0) {
+    return "The line is currently empty.";
+  }
+  let string ="The line is currently: "
+  for (var i = 0; i < crentLine.length; i++) {
+    string += `${crentLine.length}. ${crentLine[i]}, `;
+  }
+  return string;
 }
